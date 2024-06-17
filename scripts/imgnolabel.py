511688d91e96644cebc00bee904272a6ef2d072b
ybli <li_yi_bing@outlook.com>
2024-06-17 19:38:43 +0800

    feat：处理图像支持jpeg、png

--- scripts/imgnolabel.py
+++ scripts/imgnolabel.py
@@ -8,20 +8,27 @@ import gradio as gr
 def check_and_delete_images(folder_path):
     # 获取文件夹中的所有文件
     files = os.listdir(folder_path)
-    # 获取所有.jpg文件
-    jpg_files = [file for file in files if file.endswith('.jpg')]
+    # 获取所有.jpg, .jpeg和.png文件
+    image_files = [file for file in files if file.endswith(('.jpg', '.jpeg', '.png'))]
     # 获取所有.xml文件
     xml_files = [file for file in files if file.endswith('.xml')]
     # 删除文件计数
     num = 0
-    # 遍历所有.jpg文件
-    for jpg_file in jpg_files:
+    # 遍历所有图像文件
+    for image_file in image_files:
         # 检查是否存在相应的.xml文件
-        xml_file = jpg_file.replace('.jpg', '.xml')
+        # 根据图像文件的扩展名来确定.xml文件的名称
+        if image_file.lower().endswith(('.jpg', '.jpeg')):
+            xml_file = image_file.replace('.jpg', '.xml').replace('.jpeg', '.xml')
+        elif image_file.lower().endswith('.png'):
+            xml_file = image_file.replace('.png', '.xml')
+        else:
+            continue
+
         if xml_file not in xml_files:
             num += 1
-            # 如果不存在，删除.jpg文件
-            os.remove(os.path.join(folder_path, jpg_file))
+            # 如果不存在，删除图像文件
+            os.remove(os.path.join(folder_path, image_file))
 
     return f"处理完成，删除多余图像{num}个"
 
--- scripts/labelnoimg.py
+++ scripts/labelnoimg.py
@@ -10,18 +10,22 @@ def check_and_delete_labels(folder_path):
     files = os.listdir(folder_path)
     # 获取所有.xml文件
     xml_files = [file for file in files if file.endswith('.xml')]
-    # 获取所有.jpg文件
-    jpg_files = [file for file in files if file.endswith('.jpg')]
+    # 获取所有.jpg, .jpeg和.png文件
+    image_files = [file for file in files if file.endswith(('.jpg', '.jpeg', '.png'))]
 
     # 删除文件计数
     num = 0
     # 遍历所有.xml文件
     for xml_file in xml_files:
-        # 检查是否存在相应的.jpg文件
-        jpg_file = xml_file.replace('.xml', '.jpg')
-        if jpg_file not in jpg_files:
+        # 检查是否存在相应的.jpg, .jpeg或.png文件
+        # 根据.xml文件的扩展名来确定.jpg, .jpeg或.png文件的名称
+        for image_ext in ('.jpg', '.jpeg', '.png'):
+            image_file = xml_file.replace('.xml', image_ext)
+            if image_file in image_files:
+                break
+        else:
+            # 如果不存在任何对应的图像文件，则删除.xml文件
             num += 1
-            # 如果不存在，删除.xml文件
             os.remove(os.path.join(folder_path, xml_file))
 
     return f"处理完成，删除多余标签{num}个"
